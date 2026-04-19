60b944d929c2003d0e5f5453814be2da7eafa9a3
ABO5ELE <yasskhaled55@gmail.com>
2026-04-19 21:09:13 +0200

    Add configuration file for project settings

---
+++ data/src/src/src/src/tests/config/config.py
@@ -0,0 +1,16 @@
+# config.py
+# all settings for the project in one place
+
+# dataset settings
+DATA_PATH = 'data/dataset_phishing.csv'
+TEST_SIZE = 0.2
+RANDOM_STATE = 42
+
+# model settings
+N_ESTIMATORS = 100
+MAX_ITER = 2000
+N_NEIGHBORS = 5
+
+# output settings
+FIGURES_PATH = 'figures/'
+MODEL_PATH = 'models/'
